79cddbc8f930cfce86b61d0f40cf212da0086dbc
joebyjo <joebyjo@gmail.com>
2025-06-20 02:04:58 +0000

    [automatic commit] 2025-06-20 02:04:58 UTC

--- part1/app.js
+++ part1/app.js
@@ -32,7 +32,7 @@ var app = express();
     });
 
     // Create the database if it doesn't exist
-    await connection.query('CREATE DATABASE IF NOT EXISTS testdb');
+    await connection.query('CREATE DATABASE IF NOT EXISTS DogWalkService');
     await connection.end();
 
     // Now connect to the created database
