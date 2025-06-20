f1a91eaba55a9ecf7403df265061cfbee52b75bc
joebyjo <joebyjo@gmail.com>
2025-06-20 11:16:32 +0000

    [automatic commit] 2025-06-20 11:16:32 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -63,11 +63,7 @@ router.post('/:id/apply', async (req, res) => {
 router.get('/dogs', async (req, res) => {
   try {
     const [rows] = await db.query(`
-      SELECT wr.*, d.name AS dog_name, d.size, u.username AS owner_name
-      FROM WalkRequests wr
-      JOIN Dogs d ON wr.dog_id = d.dog_id
-      JOIN Users u ON d.owner_id = u.user_id
-      WHERE wr.status = 'open'
+      SELECT dog_id
     `);
     res.json(rows);
   } catch (error) {
