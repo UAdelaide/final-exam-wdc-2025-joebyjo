312d6cacb3d8a5f3aa81b2e8dd3884d362353c44
joebyjo <joebyjo@gmail.com>
2025-06-20 08:49:44 +0000

    [automatic commit] 2025-06-20 08:49:44 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,6 +37,7 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
+  // changed from requiring email to requiring username
   const { username, password } = req.body;
 
   try {
