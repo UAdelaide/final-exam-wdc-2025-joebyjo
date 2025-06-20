95ff00d59cde1e4b0cc52d698c26f277c7da60e6
joebyjo <joebyjo@gmail.com>
2025-06-20 02:51:02 +0000

    [automatic commit] 2025-06-20 02:51:02 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -20,7 +20,11 @@ router.get('/dogs', async function(req, res, next) {
 router.get('/walkrequests/open', async function(req, res, next) {
   try {
         const [rows] = await db.query(
-            'SELECT WR.request_id, D.name AS dog_name, WR.requested_time, WR.duration_minutes, WR.location, U.username AS owner_username FROM WalkRequests WR JOIN Dogs D ON WR.dog_id = D.dog_id JOIN  Users U ON D.owner_id = U.user_id WHERE WR.status = "open"'
+            `SELECT WR.request_id, D.name AS dog_name, WR.requested_time, WR.duration_minutes, WR.location, U.username AS owner_username
+			 FROM WalkRequests WR
+			 JOIN Dogs D ON WR.dog_id = D.dog_id
+			 JOIN  Users U ON D.owner_id = U.user_id
+			 WHERE WR.status = "open"`
         );
 
         res.send(rows);
