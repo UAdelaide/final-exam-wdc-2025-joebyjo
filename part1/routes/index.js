554ff9fcea9aa4e7773ec45c74221ca93a6c7032
joebyjo <joebyjo@gmail.com>
2025-06-20 02:50:42 +0000

    [automatic commit] 2025-06-20 02:50:41 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -5,7 +5,9 @@ const db = require('../services/db');
 router.get('/dogs', async function(req, res, next) {
     try {
         const [rows] = await db.query(
-            'SELECT D.name AS dog_name, D.size, U.username AS owner_username FROM Dogs D JOIN Users U ON D.owner_id = U.user_id'
+            `SELECT D.name AS dog_name, D.size, U.username AS owner_username
+			 FROM Dogs D
+			 INNER JOIN Users U ON D.owner_id = U.user_id`
         );
 
         res.send(rows);
