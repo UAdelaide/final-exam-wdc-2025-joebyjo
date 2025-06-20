bb19d132c6d51c12604034bed049ef43e2d277f1
joebyjo <joebyjo@gmail.com>
2025-06-20 08:50:47 +0000

    [automatic commit] 2025-06-20 08:50:47 UTC

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -44,12 +44,13 @@ router.post('/login', async (req, res) => {
     const [rows] = await db.query(`
       SELECT user_id, username, role FROM Users
       WHERE username = ? AND password_hash = ?
-    `, [username, password]); // updated 
+    `, [username, password]); // updated query to search by username
 
     if (rows.length === 0) {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+    // added user object to sessions
     req.session.user = rows[0];
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
