4abb526af162a2548f41885dca523d775fef234c
joebyjo <joebyjo@gmail.com>
2025-06-20 02:14:13 +0000

    [automatic commit] 2025-06-20 02:14:13 UTC

--- part1/app.js
+++ part1/app.js
@@ -3,6 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 // const db = require('./services/db');
+var mysql = require('mysql2/promise');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
