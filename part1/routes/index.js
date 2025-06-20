abc4907cb9aa57937970c13821a714110bcc841b
joebyjo <joebyjo@gmail.com>
2025-06-20 03:09:41 +0000

    [automatic commit] 2025-06-20 03:09:40 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -17,6 +17,23 @@ router.get('/dogs', async function (req, res, next) {
 });
 
 
+router.get('/walkrequests/open', async function (req, res, next) {
+    try {
+        const [rows] = await db.query(
+            `SELECT WR.request_id, D.name AS dog_name, WR.requested_time, WR.duration_minutes, WR.location, U.username AS owner_username
+            FROM WalkRequests WR
+            INNER JOIN Dogs D ON WR.dog_id = D.dog_id
+            INNER JOIN  Users U ON D.owner_id = U.user_id
+            WHERE WR.status = "open"`
+        );
+
+        res.send(rows);
+    } catch (err) {
+        res.status(500).json({ msg: `Error retrieving: ${err}` });
+    }
+});
+
+
 router.get('/walkers/summary', async function (req, res, next) {
     try {
         const [rows] = await db.query(
