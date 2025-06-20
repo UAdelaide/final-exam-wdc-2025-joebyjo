2cc4678431039a4ab426424d83579928ddd7ba20
joebyjo <joebyjo@gmail.com>
2025-06-20 02:40:36 +0000

    [automatic commit] 2025-06-20 02:40:36 UTC

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -2,9 +2,17 @@ var express = require('express');
 var router = express.Router();
 const db = require('../services/db');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/dogs', async function(req, res, next) {
+    try {
+        const [rows] = await db.query(
+            'SELECT D.name AS dog_name, D.size, U.username AS owner_username FROM Dogs D JOIN Users U ON D.owner_id = U.user_id'
+        );
+
+        res.send(rows);
+    } catch (err) {
+        res.status(500).json({ msg: `Error retrieving: ${err}`});
+    }
 });
+
 
 module.exports = router;
