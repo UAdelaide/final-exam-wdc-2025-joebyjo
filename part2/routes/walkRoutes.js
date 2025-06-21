21945c4e3348aae9a8e0c58858aafbe9d3874073
joebyjo <joebyjo@gmail.com>
2025-06-21 03:40:48 +0000

    [automatic commit] 2025-06-21 03:40:48 UTC

--- part2/routes/walkRoutes.js
+++ part2/routes/walkRoutes.js
@@ -62,7 +62,7 @@ router.post('/:id/apply', async (req, res) => {
 // added route to get all registered dogs
 router.get('/dogs', async function (req, res, next) {
     try {
-
+        // getting dogs from db
         const [rows] = await db.query(
             `SELECT D.dog_id, D.name AS dog_name, D.size, D.owner_id
             FROM Dogs D`
